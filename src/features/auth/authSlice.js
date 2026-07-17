87baa1c890853e4a8c718c41c1f6dfa91172d818
Apoorv - Patel <apoorvpatel3277@gmail.com>
2026-07-17 07:19:37 +0530

    added authSlice

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,6 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+let initialState = {
+    user : null ,
+    loading : false ,
+    error : null 
+}
+
 let authSlice = createSlice({
     name : "auth",
+    initialState , 
+    reducers : {
+        registerRequest : (state)=>{
+            state.loading = true ,
+            state.error = null
+        },
+        registerSuccess : (state , action)=>{
+            state.loading = false,
+            state.user = action.payload 
+        },
+        registerFailure : (state , action)=>{
+            state.loading = false ,
+            state.error = action.payload
+        }
+
+    }
     
 })
+
+export let {registerRequest , registerSuccess , registerFailure} = authSlice.actions
+
+export default authSlice.reducer
